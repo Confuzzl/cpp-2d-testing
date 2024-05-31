b1cff6dbdfaf4c4156c2e9e8874316b2df05b8e9
Confuzzl <fuzzltheduzzl@gmail.com>
2024-05-31 16:35:06 -0400

    maintenance

--- src/gl_wrapper/vbo_heap.cpp
+++ src/gl_wrapper/vbo_heap.cpp
@@ -4,6 +4,8 @@ module;
 
 module vbo_heap;
 
+VBOHeap::VBOHandle::~VBOHandle() { chunk->free(this); }
+
 VBOHeap::VBOChunk::free_list::free_list() {
   free_block *block = new free_block{nullptr, nullptr, 0, BLOCK_SIZE};
   begin = block;
@@ -18,41 +20,91 @@ VBOHeap::VBOChunk::free_list::~free_list() {
   }
 }
 
+void VBOHeap::VBOChunk::free_list::setBegin(free_block *block) {
+  block->prev = nullptr;
+  block->next = begin;
+  begin->prev = block;
+  begin = block;
+}
+void VBOHeap::VBOChunk::free_list::setEnd(free_block *block) {
+  block->next = nullptr;
+  block->prev = end;
+  end->next = block;
+  end = block;
+}
+
 VBOHeap::VBOChunk::free_list::free_block *
 VBOHeap::VBOChunk::free_list::prepend(const GLintptr offset,
                                       const GLsizeiptr size) {
-  free_block *block = new free_block{end, nullptr, offset, size};
-  begin->prev = block;
-  begin = block;
+  free_block *block = new free_block{nullptr, nullptr, offset, size};
+  setBegin(block);
   return block;
 }
 VBOHeap::VBOChunk::free_list::free_block *
 VBOHeap::VBOChunk::free_list::append(const GLintptr offset,
                                      const GLsizeiptr size) {
   free_block *block = new free_block{end, nullptr, offset, size};
-  end->next = block;
-  end = block;
+  setEnd(block);
   return block;
 }
 VBOHeap::VBOChunk::free_list::free_block *
 VBOHeap::VBOChunk::free_list::insertAfter(free_block *before,
                                           const GLintptr offset,
                                           const GLsizeiptr size) {
-  free_block *block = new free_block{end, nullptr, offset, size};
-  before->next->prev = block;
-  before->next = block;
+  free_block *block = new free_block{nullptr, nullptr, offset, size};
+  if (before == end) {
+    setEnd(block);
+  } else {
+    before->next = block;
+    block->prev = before;
+    free_block *after = before->next;
+    block->next = after;
+    after->prev = block;
+  }
   return block;
 }
+VBOHeap::VBOChunk::free_list::free_block *
+VBOHeap::VBOChunk::free_list::insertBefore(free_block *after,
+                                           const GLintptr offset,
+                                           const GLsizeiptr size) {
+  free_block *block = new free_block{nullptr, nullptr, offset, size};
+  if (after == begin) {
+    setBegin(block);
+  } else {
+    after->prev = block;
+    block->next = after;
+    free_block *before = after->prev;
+    block->prev = before;
+    before->next = block;
+  }
+  return block;
+}
+
+VBOHeap::VBOChunk::free_list::free_block *
+VBOHeap::VBOChunk::free_list::coalesce(free_block *block) {
+  free_block *before = block->prev, *after = block->next;
+  if (before && before->offset + before->size == block->offset) {
+    block->offset = before->offset;
+    block->size += before->size;
+    before->prev->next = block;
+    delete before;
+  }
+  if (after && block->offset + block->size == after->offset) {
+    block->size += after->size;
+    after->next->prev = block;
+    delete after;
+  }
+  return block;
+}
+
 void VBOHeap::VBOChunk::free_list::popFront() { remove(begin); }
 void VBOHeap::VBOChunk::free_list::popBack() { remove(end); }
 void VBOHeap::VBOChunk::free_list::remove(free_block *block) {
   free_block *prev = block->prev, *next = block->next;
   if (block == begin) {
-    next->prev = nullptr;
-    begin = next;
+    setBegin(next);
   } else if (block == end) {
-    prev->next = nullptr;
-    end = prev;
+    setEnd(prev);
   } else {
     prev->next = next;
     next->prev = prev;
@@ -60,28 +112,51 @@ void VBOHeap::VBOChunk::free_list::remove(free_block *block) {
   delete block;
 }
 
-VBOHeap::VBOChunk::VBOChunk() : freeList{}, blockList{} {
+VBOHeap::VBOChunk::VBOChunk() : freeList{} {
   glCreateBuffers(1, &ID);
   glNamedBufferStorage(ID, BLOCK_SIZE, NULL, GL_DYNAMIC_STORAGE_BIT);
 }
 
-const VBOHeap::VBOBlock *VBOHeap::VBOChunk::get(const GLsizeiptr size) {
+std::optional<VBOHeap::VBOHandle>
+VBOHeap::VBOChunk::get(const GLsizeiptr size) {
   free_list::free_block *current = freeList.begin;
   while (current) {
-    if (size > current->size) {
+    GLsizeiptr newSize = current->size - size;
+    if (newSize < 0) {
       current = current->next;
       continue;
     }
-    GLsizeiptr newSize = current->size - size;
+
+    VBOHandle handle{this, current->offset, size};
+    if (newSize == 0) {
+      freeList.remove(current);
+    } else {
+      current->offset += size;
+      current->size = newSize;
+    }
+    return handle;
   }
-  return nullptr;
+  return std::nullopt; // no frees
+}
+void VBOHeap::VBOChunk::free(VBOHandle *handle) {
+  const auto [chunk, offset, size] = *handle;
+  free_list::free_block *before = freeList.begin;
+  // insert new free after free with the largest offset smaller than handle
+  // offset
+  while (before->next && before->next->offset < offset) {
+    before = before->next;
+  }
+  free_list::free_block *block = freeList.insertBefore(before, offset, size);
+  freeList.coalesce(block);
 }
 
-const VBOHeap::VBOBlock &VBOHeap::get(const GLsizeiptr size) {
+VBOHeap::VBOHeap() : chunks{} {}
+
+VBOHeap::VBOHandle VBOHeap::get(const GLsizeiptr size) {
   for (VBOChunk &chunk : chunks) {
-    const VBOBlock *block = chunk.get(size);
-    if (block)
-      return *block;
+    std::optional<VBOHandle> handle = chunk.get(size);
+    if (handle)
+      return *handle;
   }
   VBOChunk &newChunk = chunks.emplace_back();
   return *newChunk.get(size);
--- src/gl_wrapper/vbo_heap.ixx
+++ src/gl_wrapper/vbo_heap.ixx
@@ -8,14 +8,19 @@ export module vbo_heap;
 import vector;
 import <memory>;
 import <forward_list>;
+import <optional>;
 
 export struct VBOHeap {
   static constexpr GLsizeiptr BLOCK_SIZE = 64;
 
-  struct VBOBlock {
-    GLuint vboID;
+  struct VBOChunk;
+
+  struct VBOHandle {
+    VBOChunk *chunk;
     GLintptr offset;
     GLsizeiptr size;
+
+    ~VBOHandle();
   };
 
   struct VBOChunk {
@@ -33,25 +38,32 @@ export struct VBOHeap {
       free_list();
       ~free_list();
 
+      void setBegin(free_block *block);
+      void setEnd(free_block *block);
+
       free_block *prepend(const GLintptr offset, const GLsizeiptr size);
       free_block *append(const GLintptr offset, const GLsizeiptr size);
       free_block *insertAfter(free_block *before, const GLintptr offset,
                               const GLsizeiptr size);
+      free_block *insertBefore(free_block *after, const GLintptr offset,
+                               const GLsizeiptr size);
+
+      free_block *coalesce(free_block *block);
+
       void popFront();
       void popBack();
       void remove(free_block *block);
     } freeList;
 
-    std::vector<VBOBlock> blockList;
-
     VBOChunk();
 
-    const VBOBlock *get(const GLsizeiptr size);
+    std::optional<VBOHandle> get(const GLsizeiptr size);
+    void free(VBOHandle *handle);
   };
 
   std::vector<VBOChunk> chunks;
 
   VBOHeap();
 
-  const VBOBlock &get(const GLsizeiptr size);
+  VBOHandle get(const GLsizeiptr size);
 };
