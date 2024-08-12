3b53d12aea4ac3074ae64497225dee915e7f2d92
Confuzzl <fuzzltheduzzl@gmail.com>
2024-08-11 22:44:41 -0400

    maintenance

--- src/world/collision/collision.cpp
+++ src/world/collision/collision.cpp
@@ -12,16 +12,13 @@ Polygon Polygon::from(geometry::Polygon &&polygon) {
   const auto &vertices = polygon.getVertices();
   const auto size = vertices.size();
 
-  bool positive = false;
   for (auto i = 0; i < size; i++) {
     const glm::vec2 a = vertices[i];
     const glm::vec2 b = vertices[(i + 1) % size];
     const glm::vec2 c = vertices[(i + 2) % size];
     const float z = (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x);
 
-    if (i == 0)
-      positive = z > 0;
-    else if (positive != (z > 0))
+    if (z < 0)
       throw std::runtime_error{"POLYGON IS NOT CONVEX"};
   }
   return fromUnchecked(std::move(polygon));
